c334429ff24b151f47aaa224f8b6ccc33ca28475
asdfwyay <unconfigured@null.spigotmc.org>
2026-01-14 23:22:59 -0500

    add version to main menu

--- client_mod/mods-unpacked/asdfwyay-APBuckshot/scripts/hooks/MenuManager.hooks.gd
+++ client_mod/mods-unpacked/asdfwyay-APBuckshot/scripts/hooks/MenuManager.hooks.gd
@@ -10,7 +10,21 @@ func Intro(chain: ModLoaderHookChain):
 	var mainNode := chain.reference_object as MenuManager
 	var mp_button = mainNode.get_node("../../Camera/dialogue UI/menu ui/main screen/button_multiplayer")
 	mp_button.text = "ARCHIPELAGO"
+	
 	chain.execute_next()
+	
+	var ap_version = Label.new()
+	
+	ap_version.text = "v0.0.3 (APBuckshot)"
+	ap_version.position = Vector2(19, 500)
+	
+	var f = load("res://fonts/fake receipt.otf")
+	ap_version.add_theme_font_override("font", f)
+	ap_version.add_theme_font_size_override("font_size", 12)
+	ap_version.add_theme_color_override("font_color", Color.DARK_GRAY)
+	ap_version.add_theme_color_override("font_shadow_color", Color.BLACK)
+	
+	mainNode.version.get_parent().add_child(ap_version)
 	
 func Start(chain: ModLoaderHookChain):
 	var mainNode := chain.reference_object as MenuManager
