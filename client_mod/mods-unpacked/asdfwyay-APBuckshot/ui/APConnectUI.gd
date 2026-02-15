33f35dbde4887b672e67562c3682609c56f4b9e1
asdfwyay <unconfigured@null.spigotmc.org>
2026-02-15 03:18:25 -0500

    add error notifications to connect UI

--- client_mod/mods-unpacked/asdfwyay-APBuckshot/ui/APConnectUI.gd
+++ client_mod/mods-unpacked/asdfwyay-APBuckshot/ui/APConnectUI.gd
@@ -7,18 +7,30 @@ var ApClient
 @onready var port_input: LineEdit = $BaseMenu/VBoxContainer/GridContainer/port_input
 @onready var password_input: LineEdit = $BaseMenu/VBoxContainer/GridContainer/password_input
 @onready var deathlink_cb: CheckButton = $BaseMenu/VBoxContainer/GridContainer/deathlink_cb
-
+@onready var connect_button: Button = $BaseMenu/VBoxContainer/GridContainer/connect_button
 @onready var connect_status: Label = $BaseMenu/VBoxContainer/GridContainer/connect_status
+
+@onready var error_msg: Label = $ErrorNotification/error_msg
+
+var canConnect: bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ApClient = $"/root/ModLoader/asdfwyay-APBuckshot/ApClient"
 	print(ApClient)
+	ApClient.send_error.connect(_on_receive_error_msg)
+	
+	canConnect = true
 	
 	slot_input.text = ApClient.slot
 	host_input.text = ApClient.hostname
 	port_input.text = ApClient.port
 	password_input.text = ApClient.password
+	
+	error_msg.text = ""
+	error_msg.modulate.a = 0.0
+	
+	connect_status.text = "NOT CONNECTED"
 	
 	deathlink_cb.set_pressed_no_signal(ApClient.deathLink)
 
@@ -27,10 +39,13 @@ func _process(delta):
 	match ApClient.connectionState:
 		ApClient.ConnectionState.DISCONNECTED:
 			connect_status.text = "NOT CONNECTED"
+			connect_button.text = "Connect"
 		ApClient.ConnectionState.CONNECTING:
 			connect_status.text = "CONNECTING..."
+			connect_button.text = "Connect"
 		ApClient.ConnectionState.CONNECTED:
 			connect_status.text = "CONNECTED"
+			connect_button.text = "Disconnect"
 			
 
 func remove_char_at_index(in_str: String, i: int) -> String:
@@ -93,10 +108,39 @@ func _on_connect_button_pressed():
 	var port: String = port_input.text
 	var password: String = password_input.text
 	
-	ApClient.APConnect(slot, hostname, port, password)
+	if (
+		ApClient.socket.get_ready_state() == WebSocketPeer.STATE_OPEN
+		and connect_button.text == "Disconnect"
+	):
+		ApClient.attemptReconnection = false
+		ApClient.socket.close()
+		canConnect = true
+	elif canConnect:
+		canConnect = false
+		await ApClient.APConnect(slot, hostname, port, password)
+		canConnect = true
+
+func display_error_msg(msg: String, duration: float, fade_out_duration: float) -> void:
+	error_msg.text = msg
+	error_msg.modulate.a = 1.0
+	
+	await get_tree().create_timer(duration).timeout
+	
+	var tween = create_tween()
+	tween.tween_property(error_msg, "modulate:a", 0.0, fade_out_duration)
 
 func _on_close_button_pressed():
 	queue_free()
 
 func _on_deathlink_cb_toggled(button_pressed):
 	ApClient.setDeathLink(deathlink_cb.button_pressed)
+
+func _on_receive_error_msg(msg):
+	canConnect = true
+	match ApClient.latest_error_msg:
+		"InvalidSlot":
+			display_error_msg("Invalid Slot", 4.0, 1.0)
+		"InvalidPassword":
+			display_error_msg("Invalid Password", 4.0, 1.0)
+		_:
+			display_error_msg("Failed to Connect", 4.0, 1.0)
